4b401071015629e2e3406210c7ca6e811a986fc2
Jonathan Martel <jonathmartel@gmail.com>
2021-08-19 16:41:19 -0400

    Avec le router

--- js/main.mjs
+++ js/main.mjs
@@ -1,6 +1,7 @@
 import _App from './App.mjs';
 import Tache from './Tache.mjs';
 import Affichage from './Affichage.mjs';
+import page from "//unpkg.com/page/page.mjs";
 
 (function(){
     let App = {};
@@ -9,5 +10,17 @@ import Affichage from './Affichage.mjs';
         //Tache.setUsager();
         Affichage.afficheEnregistrer();
         Tache.logUsager();
+
+        page("/", ()=>{console.log("page d'accueil")});
+        page("/tache", ()=>{console.log("page tache")});
+        page("/tache/:id", (ctx)=>{console.log(ctx)});
+        page("/connecter", ()=>{console.log("page de login")});
+        page("/enregistrer", ()=>{console.log("Enregistrement")});
+        page("/ajouter", ()=>{console.log("ajouter")});
+        page("*", ()=>{console.log("non trouvé")});
+        page({
+            hashbang : true
+        });
+
     })
 })()
